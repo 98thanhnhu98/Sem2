e4a17323dbf49a547815c1c12352644247b4860d
98thanhnhu98 <97713475+98thanhnhu98@users.noreply.github.com>
2022-06-01 09:49:19 +0700

    Bai tap Lab7 cua Do Minh Thanh

---
+++ src/main/java/JPL0Lab7/StudentList.java
@@ -0,0 +1,84 @@
+package JPL0Lab7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class StudentList {
+    private ArrayList<Student> list = new ArrayList<>();
+    private int length;
+
+    public StudentList(){
+        list.add(new Student(0,"minh","thành",9.5d));
+        list.add(new Student(1,"cao","thắng",8d));
+        list.add(new Student(2,"minh","quân",10d));
+        list.add(new Student(3,"lê","vũ",7.3d));
+    }
+    public ArrayList<Student> findByName(String name){
+        boolean found = false;
+        ArrayList<Student> matches = new ArrayList<>();
+        for (Student s : list){
+            String fullName = new String(s.getFirstName() + " " + s.getLastName()).toLowerCase();
+            if (fullName.matches("(.*)" + name.toLowerCase() + "(.*)")){
+                matches.add(s);
+                found = true;
+            }
+        }
+        if (!found){
+            System.out.println("Not Found");
+        }
+        return matches;
+    }
+
+    public Student findById (int id){
+        for (Student s : list){
+            if (s.getId() == id){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public void add(Student s){
+        list.add(s);
+    }
+
+    public void remove(int id){
+        boolean found = false;
+        for (Student s :list){
+            if (s.getId() == id){
+                int choice;
+                System.out.println("Are u sure deleting this student ? (1.Yes 2.No");
+                choice = new Scanner(System.in).nextInt();
+                if (choice == 1){
+                    list.remove(s);
+                }
+            }
+            if (!found){
+                System.out.println("Can not find student with id " + id);
+            }
+        }
+    }
+
+    public void sortByMark(){
+        Collections.sort(list, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return Double.compare(o2.getMark(), o1.getMark());
+            }
+        });
+    }
+
+    public void showList(){
+        for (Student s : list){
+            s.printInfo();
+        }
+    }
+
+    public void showList(ArrayList<Student> list){
+        for (Student s : list){
+            s.printInfo();
+        }
+    }
+}
